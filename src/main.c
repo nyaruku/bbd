bdb8bc6810b5376ba02fe0e90d8f0863b2fd2e57
nyaruku <38096703+nyaruku@users.noreply.github.com>
2026-08-13 00:24:58 +0200

    Basic raylib implementation with software rendering

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
-#include <stdio.h>
+#include "raylib.h"
 
-int main()
+int main(void)
 {
-    printf("Hello World\n");
+    InitWindow(800, 450, "raylib example - basic window");
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("Batch Beatmap Downloader", 190, 200, 20, LIGHTGRAY);
+        DrawText(TextFormat("Frame Time: %.1f", GetFrameTime()*1000), 190, 230, 20, LIGHTGRAY);
+        DrawText(TextFormat("FPS: %i", GetFPS()), 190, 260, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
